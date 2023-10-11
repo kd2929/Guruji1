916b41e01103a4137a63d829c7771f5de342f289
Lord <110597375+kd2929@users.noreply.github.com>
2023-10-11 09:26:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,7 @@ bot = Client(
     bot_token=os.environ.get("BOT_TOKEN"),
     api_id=int(os.environ.get("API_ID")),
     api_hash=os.environ.get("API_HASH")
-
-    
+)
 
 @bot.on_message(filters.command(["pyro"]))
 async def account_login(bot: Client, m: Message):
